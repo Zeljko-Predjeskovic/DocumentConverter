461cb8889873c5edca04854fe62d5c82bff6637b
amarek <alexander.marek@outlook.com>
2023-03-08 11:28:56 +0100

    PdfToSvgConverter does not dispose streams

---
+++ tests/DocumentConverter.Tests/PdfToSvgConverterTests.cs
@@ -0,0 +1,47 @@
+﻿using Shouldly;
+using Svg;
+
+namespace DocumentConverter.Tests
+{
+    public class PdfToSvgConverterTests
+    {
+
+        private IPdfToSvgConverter _converter;
+
+        [SetUp]
+        public void Setup()
+        {
+            SvgPlatform.Init();
+
+            _converter = new PdfToSvgConverter();
+        }
+
+        [Test]
+        public async Task DoesNotDisposeStreamWhenConverting()
+        {
+            // Arrange
+            using var input = new TestableStream();
+            using var output = new TestableStream();
+            var filePath = ".\\Resources\\Vienna.pdf";
+            var file = File.OpenRead(filePath);
+            file.CopyTo(input);
+
+            // Act
+            await _converter.ConvertPdfToSvgAsync(input, output, CancellationToken.None);
+
+            // Assert
+            input.IsDisposed.ShouldBeFalse();
+            output.IsDisposed.ShouldBeFalse();
+        }
+
+        private class TestableStream : MemoryStream
+        {
+            public bool IsDisposed { get; set; }
+            protected override void Dispose(bool disposing)
+            {
+                base.Dispose(disposing);
+                IsDisposed = true;
+            }
+        }
+    }
+}
